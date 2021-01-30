b9fbf35f34f7b4cd818cf7c91b4e442d3870b817
BBoyTukytuk <63485300+BBoyTukytuk@users.noreply.github.com>
2021-01-30 15:05:32 +0900

    Add files via upload

---
+++ question.js
@@ -0,0 +1,30 @@
+
+
+//Array of Objects
+const quiz = [
+{
+ q:'Which one is not a financial statement?',
+ options:['Income Statement','Cashflow Statement','Balance Sheet','Profit and Gains Statement'],
+ answer:0
+},
+{
+  q:'What color is a bannana ?',
+  options:['red','yellow','white','blue'],
+  answer:1
+ },
+ {
+  q:'3 + 4 = 7 ?',
+  options:['true','false'],
+  answer:0
+ },
+ {
+  q:'What time of the day do we have breakfast ?',
+  options:['In the afternoon','In the evening', 'In the morning'],
+  answer:2
+ },
+ {
+  q:'What is 22 + 6?',
+  options:['99','56', '16','28'],
+  answer:3
+ }
+]
